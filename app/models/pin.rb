b02156cca9b2d6aefd1a1e684e5ae775c28754ee
funnythingz <funnythingz69+github@gmail.com>
2014-11-03 12:01:37 +0900

    config carrierwave

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,3 @@
 class Pin < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,18 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+
+  process :resize_to_fill => [1024, 768]
+  version :thumb do
+    process :resize_to_fill => [300, 300]
+  end
+
+  def extension_white_list
+    %w(jpg jpeg)
+  end
+
+  process convert: 'jpg'
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -14,6 +25,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def filename
+    "#{Time.now.strftime('%Y%m%d%H%M%S')}.jpg"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
