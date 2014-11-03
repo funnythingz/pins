10e294f246d6c3eee5d39ff7a65129bf04affa09
funnythingz <funnythingz69+github@gmail.com>
2014-11-03 13:28:54 +0900

    tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   root 'pingo#stream'
 
   # pingo
-  get '/pingo(.:format)', to: 'pingo#index', as: 'pingo_index'
-  post '/pingo(.:format)', to: 'pingo#create'
-  get '/pingo/new(.:format)', to: 'pingo#new', as: 'new_pingo'
-  get '/pingo/:id/edit(.:format)', to: 'pingo#edit', as: 'edit_pingo'
-  get '/pingo/:id(.:format)', to: 'pingo#show', as: 'pingo'
-  patch '/pingo/:id(.:format)', to: 'pingo#update'
-  delete '/pingo/:id(.:format)', to: 'pingo#destroy'
+  get     '/pingo(.:format)',           to: 'pingo#index',  as: 'pingo_index'
+  post    '/pingo(.:format)',           to: 'pingo#create'
+  get     '/pingo/new(.:format)',       to: 'pingo#new',    as: 'new_pingo'
+  get     '/pingo/:id/edit(.:format)',  to: 'pingo#edit',   as: 'edit_pingo'
+  get     '/pingo/:id(.:format)',       to: 'pingo#show',   as: 'pingo'
+  patch   '/pingo/:id(.:format)',       to: 'pingo#update'
+  delete  '/pingo/:id(.:format)',       to: 'pingo#destroy'
 
 end
