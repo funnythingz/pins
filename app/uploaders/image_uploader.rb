a3477566374c32bad00e5dea9710a413d0f0c167
funnythingz <funnythingz69+github@gmail.com>
2014-11-03 20:37:00 +0900

    changed resize_to_fit => resize_to_fill

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :icon do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fill => [100, 100]
   end
 
   def extension_white_list
