cd1a1c7f9f95e027c29f8096312dd3be56756a6e
funnythingz <funnythingz69+github@gmail.com>
2014-11-03 22:43:43 +0900

    added full_title method in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     str = html_escape(str)
     str.gsub(/\r\n|\r|\n/, "<br>").html_safe
   end
+
+  def full_title(title)
+    if title.blank?
+      return Settings.site_information.title
+    end
+
+    return "#{title} | #{Settings.site_information.title}"
+  end
 end
