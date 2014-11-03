9d7be33e3269eada35711541667a6090cebb4d41
funnythingz <funnythingz69+github@gmail.com>
2014-11-03 17:08:49 +0900

    modified migrate file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    if user.member?
+      can :member, :all
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
--- db/migrate/20141103075749_add_role_to_member_users.rb
+++
@@ -1,5 +0,0 @@
-class AddRoleToMemberUsers < ActiveRecord::Migration
-  def change
-    add_column :member_users, :role, :string
-  end
-end
