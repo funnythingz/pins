5246a8cc4ae1b1f8118a05f1242751d8a6e84e07
funnythingz <funnythingz69+github@gmail.com>
2014-11-03 21:34:16 +0900

    added my of routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,14 @@ Rails.application.routes.draw do
   root 'pin#stream'
 
   # pin
-  get     '/pin(.:format)',           to: 'pin#index',  as: 'pins'
   post    '/pin(.:format)',           to: 'pin#create'
   get     '/pin/new(.:format)',       to: 'pin#new',    as: 'new_pin'
   get     '/pin/:id/edit(.:format)',  to: 'pin#edit',   as: 'edit_pin'
   get     '/pin/:id(.:format)',       to: 'pin#show',   as: 'pin'
   patch   '/pin/:id(.:format)',       to: 'pin#update'
   delete  '/pin/:id(.:format)',       to: 'pin#destroy'
+
+  # my
+  get     '/my',                      to: 'my#view',    as: 'my'
 
 end
