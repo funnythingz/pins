cf7701333452a729e55784ea9d61f06425e799ac
funnythingz <funnythingz69+github@gmail.com>
2014-11-03 20:20:19 +0900

    added create pin

--- app/controllers/pin_controller.rb
+++ app/controllers/pin_controller.rb
@@ -1,4 +1,5 @@
 class PinController < ApplicationController
+
   def permit_pingo_params
     params.require(:pin).permit(:image)
   end
@@ -8,10 +9,33 @@ class PinController < ApplicationController
   end
 
   def new
-    @user_id
+    if current_user
+      @user_id = current_user.id
+      render :new
+    else
+      redirect_to login_path
+    end
   end
 
   def create
+    @pin = Pin.new(pin_params)
 
+    respond_to do |format|
+      if @pin.save
+        format.html { redirect_to @pin, notice: 'ok' }
+      else
+        format.html { render :new }
+      end
+    end
   end
+
+  private
+  def set_pin
+    @pin = Pin.find(params[:id])
+  end
+
+  def pin_params
+    params.require(:pin).permit(:title, :description, :image, :user_id)
+  end
+
 end
