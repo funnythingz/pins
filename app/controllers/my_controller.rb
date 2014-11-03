a180db0ca8f6108067dc3811adf696563b6e45da
funnythingz <funnythingz69+github@gmail.com>
2014-11-03 21:36:27 +0900

    rails g controller my view

---
+++ app/controllers/my_controller.rb
@@ -0,0 +1,4 @@
+class MyController < ApplicationController
+  def view
+  end
+end
---
+++ spec/views/my/view.html.slim_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "my/view.html.slim", :type => :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
