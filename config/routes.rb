352ff7f8558d4a352c4d4e6c101389642f5d8969
funnythingz <funnythingz69+github@gmail.com>
2014-11-03 16:53:14 +0900

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  # devise
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_scope :user do
+    get "/login", to: "devise/sessions#new"
+    get "/logout", to: "devise/sessions#destroy"
+  end
+
   root 'pin#stream'
 
   # pin
