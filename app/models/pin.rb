19e95512da342b36fdec84e013a5b4e9f588a229
funnythingz <funnythingz69+github@gmail.com>
2014-11-06 02:02:46 +0900

    added file_size_validator

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,10 +1,24 @@
+require 'file_size_validator'
+
 class Pin < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
   validates :title, length: { maximum: 50 }, presence: true
   validates :description, length: { maximum: 1000}, presence: true
-  validates :image, presence: true
+  validates :image,
+    presence: true,
+    file_size: {
+      :maximum => 10.megabytes.to_i
+    }
+
+  # TODO: 画像サイズのバリデーション
+  # validate :check_image_dimensions
+
+  # def check_image_dimensions
+  #   Rails.logger.info "Image upload dimensions: #{image.geometry[:width]}x#{image.geometry[:height]}"
+  #   errors.add :image, '400x400ピクセル以上のサイズの画像をアップロードしてください' if image.geometry[:width] < 400 || image.geometry[:height] < 400
+  # end
 
   scope :get_pin, ->(id) {
     Pin.find_by(id: id)
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
+  process :resize_to_limit => [800, 800]
   process :resize_to_fit => [1024, 768]
 
   version :thumb do
