737d4d76bb8bad1aaf379dd08e3c799f73782fc2
funnythingz <funnythingz69+github@gmail.com>
2014-11-06 01:20:24 +0900

    modified use current_user

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -4,6 +4,6 @@ class MyController < ApplicationController
 
   def view
     @my = current_user
-    @pins = Pin.where(user_id: @my.id).order(created_at: 'DESC')
+    @pins = Pin.where(user_id: current_user.id).order(created_at: 'DESC')
   end
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,12 +5,6 @@ class Pin < ActiveRecord::Base
   validates :title, length: { maximum: 50 }, presence: true
   validates :description, length: { maximum: 1000}, presence: true
   validates :image, presence: true
-  validate :check_image_dimensions
-
-  def check_image_dimensions
-    ::Rails.logger.info "Image upload dimensions: #{image.geometry[:width]}x#{image.geometry[:height]}"
-    errors.add :image, '400x400ピクセル以上のサイズの画像をアップロードしてください' if image.geometry[:width] < 400 || image.geometry[:height] < 400
-  end
 
   scope :get_pin, ->(id) {
     Pin.find_by(id: id)
