c688a4c57d3c0f15882ef4ca40c9a41f70685a2f
funnythingz <funnythingz69+github@gmail.com>
2014-11-14 01:27:59 +0900

    added validation for image of Pin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,31 +2,20 @@ require 'file_size_validator'
 
 class Pin < ActiveRecord::Base
 
+  IMAGE_MIN_WIDTH  = 400
+  IMAGE_MIN_HEIGHT = 400
+
   mount_uploader :image, ImageUploader
 
   belongs_to :user
 
   validates :title, length: { maximum: 50 }, presence: true
   validates :description, length: { maximum: 1000}, presence: true
-  validates :image,
-    presence: true,
-    file_size: {
-      :maximum => 10.megabytes.to_i
-    }
 
-  # TODO: 画像サイズのバリデーション
-  # validate :check_image_dimensions
+  validate :check_image_dimensions
 
-  # def check_image_dimensions
-  #   Rails.logger.info "Image upload dimensions: #{image.geometry[:width]}x#{image.geometry[:height]}"
-  #   errors.add :image, '400x400ピクセル以上のサイズの画像をアップロードしてください' if image.geometry[:width] < 400 || image.geometry[:height] < 400
-  # end
-
-  scope :get_pin, ->(id) {
-    Pin.find_by(id: id)
-  }
-
-  scope :get_pin_params, -> (params) {
-    params.require(:pin).permit(:title, :description, :image, :user_id)
-  }
+  def check_image_dimensions
+    Rails.logger.info "Image upload dimensions: #{image.geometry[:width]}x#{image.geometry[:height]}"
+    errors.add :image, "#{IMAGE_MIN_WIDTH}x#{IMAGE_MIN_HEIGHT}ピクセル以上のサイズの画像をアップロードしてください" if image.geometry[:width] < IMAGE_MIN_WIDTH || image.geometry[:height] < IMAGE_MIN_HEIGHT
+  end
 end
