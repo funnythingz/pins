73f0185885c609f021e6676d15dc29ad26fcc9c9
funnythingz <funnythingz69+github@gmail.com>
2014-11-30 00:51:58 +0900

    migrate add culomn status to pins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104163312) do
+ActiveRecord::Schema.define(version: 20141129155104) do
 
   create_table "pins", force: true do |t|
     t.string   "title",       limit: 255
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141104163312) do
     t.integer  "user_id",     limit: 4
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.string   "status",      limit: 255
   end
 
   create_table "profiles", force: true do |t|
