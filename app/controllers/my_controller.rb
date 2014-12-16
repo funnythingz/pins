e977877187199c63a2e6ac01c81fc2a8c0356ad0
funnythingz <funnythingz69+github@gmail.com>
2014-12-16 23:56:08 +0900

    tweaks

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -3,7 +3,7 @@ class MyController < ApplicationController
   before_action :authenticate_user!
 
   def my
-    @pins = current_user.pin.order(created_at: 'DESC')
+    @pins = current_user.pin.order(created_at: :desc)
   end
 
   def favorites
