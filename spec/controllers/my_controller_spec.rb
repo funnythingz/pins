2c05d04284837d99b8156c1a7cade08662b91225
funnythingz <funnythingz69+github@gmail.com>
2014-12-21 02:03:51 +0900

    deleted spec from controller

--- spec/controllers/my_controller_spec.rb
+++ spec/controllers/my_controller_spec.rb
@@ -2,11 +2,4 @@ require 'rails_helper'
 
 RSpec.describe MyController, :type => :controller do
 
-  describe "GET my" do
-    it "returns http success" do
-      get :my
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
