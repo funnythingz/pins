476cd0f2a4e1f801eedf27ffc45288c5b79dd001
funnythingz <funnythingz69+github@gmail.com>
2014-12-22 00:03:34 +0900

    nl2br change to simple_format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,2 @@
 module ApplicationHelper
-  def nl2br(str)
-    if str.present?
-      str = html_escape(str)
-      str.gsub(/\r\n|\r|\n/, "<br>").html_safe
-    end
-  end
-
-  def full_title(title)
-    if title.blank?
-      return Settings.site_information.title
-    end
-
-    return "#{title} | #{Settings.site_information.title}"
-  end
 end
