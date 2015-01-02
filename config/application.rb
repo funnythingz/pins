df33c6e4efed56fdfb57a180a657196535e35664
funnythingz <funnythingz69+github@gmail.com>
2015-01-02 21:24:09 +0900

    renamed

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'faker'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Pingo
+module Pins
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
